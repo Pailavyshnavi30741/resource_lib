25a4261e002b314354b98058e5fc0e9a4de6dd7e
Pailavyshnavi30741 <2400030741@kluniversity.in>
2026-05-02 14:38:27 +0530

    Fixed DB config for Railway

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
